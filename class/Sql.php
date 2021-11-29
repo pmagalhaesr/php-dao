6854142eaa6a088d3502328366ffeaff53e53e14
Paulo Magalhães <pmagalhaesr@outlook.com>
2021-11-29 18:16:43 -0300

    criamos a classe usuario

--- class/Sql.php
+++ class/Sql.php
@@ -1,51 +1,52 @@
-<?php 
+<?php
 
 class Sql extends PDO {
 
-	private $conn;
+    private $conn;
 
-	public function __construct(){
+    public function __construct() {
 
-		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp", "root", "");
-		}
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp", "root", "");
 
-private function setParams($statment, $parameters = array()){
+    }
 
-	foreach ($parameters as $key => $value){
+    private function setParams($statement, $parameters = array()) {
 
-		$this->bindParam ($key, $value);
+        foreach ($parameters as $key => $value) {
 
-	}
+            $this->setParam($statement, $key, $value);
+
+        }
+
+    }
+
+    private function setParam($statement, $key, $value){
+
+        $statement->bindParam($key, $value);
+
+    }
+
+    public function run($rawQuery, $params = array()) {
+
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+
+    }
+
+    public function select($rawQuery, $params = array()):array
+    {
+
+        $stmt = $this->run($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
 
 }
 
-private function setParam($statment, $key, $value){
-
-	$statment->bindParam($key, $value);
-
-}
-
-public function PDOConnection($rawQuery, $params = array()){
-
-	$stmt = $this->conn->prepare($rawQuery);
-
-	$this->setParams($stmt, $params);
-
-	$stmt->execute();
-
-	return $stmt;
-
-	}
-
-	public function select($rawQuery, $params = array()):array
-	{
-
-		$stmt = $this->PDOConnection($rawQuery, $params);
-		
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-	}
-
-}
-
- ?>
+?>
